9076b77c80348a406ee494705a71f1df84c736b0
Yildiray Kabasakal <yd_kabasakal@hotmail.com>
2022-09-27 22:54:32 +0300

    EggTimer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,54 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
-
-
+    var eggTimer : Float? = 60
+    var timer = Timer()
+    var percentage : Float? = 0
+    var eggTimespercentage : Float? = 5
+    
+    func countDown(){
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter(){
+        if eggTimer! > 0 {
+            
+            print(eggTimer!)
+            
+            eggTimer! = eggTimer!-1
+            
+            percentage = ((eggTimespercentage!-eggTimer!)/eggTimespercentage!)
+            
+            progressBar.progress=percentage!
+            
+            print(percentage)
+            
+            
+        }
+        else{
+            timer.invalidate()
+            changeLabel.text = "Done !"
+        }
+    }
+    
+   
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var changeLabel: UILabel!
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        eggTimer = Float(eggTimes[hardness]!)
+        eggTimespercentage = Float( eggTimes[hardness]!)
+        
+        
+        countDown()
+        
+        
+        
+    }
+    
 }
